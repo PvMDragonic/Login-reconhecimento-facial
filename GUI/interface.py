9254bfee8cc9c642b8c3e2a0360639d684a2a7e9
PvMDragonic <55903030+PvMDragonic@users.noreply.github.com>
2021-10-01 12:38:14 -0300

    Versão 0.4
    Finalizando o processo de salvar os dados de quem se registrar num banco de dados.

--- GUI/interface.py
+++ GUI/interface.py
@@ -1,9 +1,12 @@
+import io
 import sys
 import cv2
 import sqlite3
+import numpy as np
 from sqlite3 import Error
 import tkinter as tk
 import tkinter.ttk as ttk
+from tkinter import IntVar
 from PIL import Image, ImageTk
 
 # Porque precisa desse . antes de 'tooltip' só deus sabe.
@@ -16,11 +19,11 @@ def iniciar_menu_inicial():
 
 class JanelaPrincipal:
     def __init__(self, top = None):
-        _bgcolor = '#d9d9d9'  # X11 color: 'gray85'
-        _fgcolor = '#000000'  # X11 color: 'black'
-        _compcolor = '#d9d9d9' # X11 color: 'gray85'
-        _ana1color = '#d9d9d9' # X11 color: 'gray85'
-        _ana2color = '#ececec' # Closest X11 color: 'gray92'
+        self._bgcolor = '#d9d9d9'  # X11 color: 'gray85'
+        self._fgcolor = '#000000'  # X11 color: 'black'
+        self._compcolor = '#d9d9d9' # X11 color: 'gray85'
+        self._ana1color = '#d9d9d9' # X11 color: 'gray85'
+        self._ana2color = '#ececec' # Closest X11 color: 'gray92'
 
         # Isso aqui tá definido como 'self' só pra eu poder definir uma única vez aqui e depois usar no resto da classe.
         self.font9 = "-family {Segoe UI Light} -size 12 -weight bold"
@@ -29,13 +32,13 @@ class JanelaPrincipal:
         self.style = ttk.Style()
         if sys.platform == "win32":
             self.style.theme_use('winnative')
-        self.style.configure('.',background = _bgcolor)
-        self.style.configure('.',foreground = _fgcolor)
+        self.style.configure('.',background = self._bgcolor)
+        self.style.configure('.',foreground = self._fgcolor)
         self.style.configure('.',font = "TkDefaultFont")
-        self.style.map('.',background = [('selected', _compcolor), ('active',_ana2color)])
+        self.style.map('.',background = [('selected', self._compcolor), ('active',self._ana2color)])
 
-        X = int(top.winfo_screenwidth()/2 - top.winfo_reqwidth()/2)
-        Y = int(top.winfo_screenheight()/3 - top.winfo_reqheight()/2)
+        X = int(top.winfo_screenwidth() / 2 - top.winfo_reqwidth() / 2)
+        Y = int(top.winfo_screenheight() / 3 - top.winfo_reqheight() / 2)
         X = int(X * 0.8)
         Y = int(Y * 0.8)
 
@@ -67,8 +70,54 @@ class JanelaPrincipal:
         button3_ttp = CreateToolTip(self.botao_sair, \
             "Clique para sair e encerrar o programa.")
     
+    def tela_de_aviso(self, frame_principal, msg):
+        self.frame_temp = ttk.Frame(self.frame_principal)
+        self.frame_temp.place(relx=0.014, rely=0.017, relheight=0.96, relwidth=0.97)
+        self.frame_temp.configure(relief='groove')
+        self.frame_temp.configure(borderwidth="2")
+        self.frame_temp.configure(relief="groove")
+
+        self.texto_mensagem = tk.Message(self.frame_temp)
+        self.texto_mensagem.place(relx = 0.309, rely = 0.285, relheight = 0.292, relwidth = 0.341)
+        self.texto_mensagem.configure(background = "#d9d9d9")
+        self.texto_mensagem.configure(font = self.font9)
+        self.texto_mensagem.configure(foreground = "#000000")
+        self.texto_mensagem.configure(highlightbackground = "#d9d9d9")
+        self.texto_mensagem.configure(highlightcolor = "black")
+        self.texto_mensagem.configure(text = msg)
+        self.texto_mensagem.configure(anchor='center')
+        self.texto_mensagem.configure(justify='center')
+        self.texto_mensagem.configure(width = 210)
+
+        self.botao_ok = ttk.Button(self.frame_temp, text = "Ok", command = self.frame_temp.destroy)
+        self.botao_ok.place(relx=0.400, rely=0.870, height=35, width=116)
+
+    def __conecxao_db(self):
+        conn = None
+        try:
+            conn = sqlite3.connect("database/pessoas.db", detect_types = sqlite3.PARSE_DECLTYPES)
+        except sqlite3.OperationalError:
+            from os import mkdir
+            mkdir('database')
+        finally:
+            conn = sqlite3.connect("database/pessoas.db", detect_types = sqlite3.PARSE_DECLTYPES)
+
+        return conn
+
     def botao_validar_clicado(self):
-        print("bruh1")
+        def convert_array(text):
+            out = io.BytesIO(text)
+            out.seek(0)
+            return np.load(out)
+
+        conn = self.__conecxao_db()
+
+        sqlite3.register_converter("array", convert_array)
+
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM registros")
+        print(cur.fetchall())
+        conn.close()
 
     def botao_registrar_clicado(self, frame_principal):
         self.texto_nome = tk.Entry(self.frame_principal)
@@ -86,17 +135,20 @@ class JanelaPrincipal:
 
         self.label_nome = tk.Label(self.frame_principal)
         self.label_nome.place(relx=0.195, rely=0.047, height=22, width=65)
+        self.label_nome.configure(activebackground="#f9f9f9")
+        self.label_nome.configure(activeforeground="black")
         self.label_nome.configure(anchor='se')
         self.label_nome.configure(background="#d9d9d9")
-        self.label_nome.configure(cursor="fleur")
         self.label_nome.configure(disabledforeground="#a3a3a3")
-        self.label_nome.configure(font=self.font9)
+        self.label_nome.configure(font="-family {Segoe UI} -size 12")
         self.label_nome.configure(foreground="#000000")
+        self.label_nome.configure(highlightbackground="#d9d9d9")
+        self.label_nome.configure(highlightcolor="black")
         self.label_nome.configure(justify='right')
         self.label_nome.configure(text='''Nome:''')
 
         self.texto_email = tk.Entry(self.frame_principal)
-        self.texto_email.place(relx=0.325, rely=0.141, height=21, relwidth=0.461)
+        self.texto_email.place(relx=0.325, rely=0.117, height=20, relwidth=0.461)
 
         self.texto_email.configure(background="white")
         self.texto_email.configure(disabledforeground="#a3a3a3")
@@ -110,7 +162,7 @@ class JanelaPrincipal:
         self.texto_email.configure(takefocus="0")
 
         self.label_email = tk.Label(self.frame_principal)
-        self.label_email.place(relx=0.195, rely=0.141, height=21, width=65)
+        self.label_email.place(relx=0.195, rely=0.117, height=21, width=65)
         self.label_email.configure(activebackground="#f9f9f9")
         self.label_email.configure(activeforeground="black")
         self.label_email.configure(anchor='se')
@@ -124,7 +176,7 @@ class JanelaPrincipal:
         self.label_email.configure(text='''E-Mail:''')
 
         self.texto_cpf = tk.Entry(self.frame_principal)
-        self.texto_cpf.place(relx=0.325, rely=0.235,height=21, relwidth=0.461)
+        self.texto_cpf.place(relx=0.325, rely=0.188,height=21, relwidth=0.461)
         self.texto_cpf.configure(background="white")
         self.texto_cpf.configure(disabledforeground="#a3a3a3")
         self.texto_cpf.configure(font="TkFixedFont")
@@ -137,7 +189,7 @@ class JanelaPrincipal:
         self.texto_cpf.configure(takefocus="0")
 
         self.label_cpf = tk.Label(self.frame_principal)
-        self.label_cpf.place(relx=0.195, rely=0.235, height=21, width=65)
+        self.label_cpf.place(relx=0.195, rely=0.188, height=21, width=65)
         self.label_cpf.configure(activebackground="#f9f9f9")
         self.label_cpf.configure(activeforeground="black")
         self.label_cpf.configure(anchor='se')
@@ -150,15 +202,36 @@ class JanelaPrincipal:
         self.label_cpf.configure(justify='right')
         self.label_cpf.configure(text='''CPF:''')
 
+        self.style.map('TRadiobutton',background=[('selected', self._bgcolor), ('active', self._ana2color)])
+
+        # Faz a opção 'opc_generico' ser o valor default; 'self.cargo_selecionado' guarda qual radiobutton foi selecionado, via os valores deles.
+        self.cargo_selecionado = IntVar()
+        self.cargo_selecionado.set(1)
+
+        self.opc_generico = ttk.Radiobutton(self.frame_principal, variable = self.cargo_selecionado, value = 1)
+        self.opc_generico.place(relx=0.292, rely=0.282, relwidth=0.112, relheight=0.0, height=21)
+        self.opc_generico.configure(text='''Genérico''')
+        self.opc_generico.configure(cursor="hand2")
+
+        self.opc_diretor = ttk.Radiobutton(self.frame_principal, variable = self.cargo_selecionado, value = 2)
+        self.opc_diretor.place(relx=0.455, rely=0.282, relwidth=0.112, relheight=0.0, height=21)
+        self.opc_diretor.configure(text='''Diretor''')
+        self.opc_diretor.configure(cursor="hand2")
+
+        self.opc_ministro = ttk.Radiobutton(self.frame_principal, variable = self.cargo_selecionado, value = 3)
+        self.opc_ministro.place(relx=0.601, rely=0.282, relwidth=0.112, relheight=0.0, height=21)
+        self.opc_ministro.configure(text='''Ministro''')
+        self.opc_ministro.configure(cursor="hand2")
+
         self.webcam_frame = tk.Frame(self.frame_principal)
-        self.webcam_frame.place(relx=0.227, rely=0.330, relheight=0.481, relwidth=0.56)
+        self.webcam_frame.place(relx=0.227, rely=0.390, relheight=0.481, relwidth=0.56)
         self.webcam_frame.configure(relief='groove')
         self.webcam_frame.configure(borderwidth="2")
         self.webcam_frame.configure(relief="groove")
         self.webcam_frame.configure(background="#d9d9d9")
 
         self.botao_tirar_foto = ttk.Button(self.frame_principal, text = "Tirar foto", command = lambda: self.botao_tirar_foto_clicado(self.frame_principal, self.botao_tirar_foto))
-        self.botao_tirar_foto.place(relx=0.428, rely=0.870, height=35, width=86)
+        self.botao_tirar_foto.place(relx=0.428, rely=0.892, height=35, width=86)
         self.botao_tirar_foto.configure(takefocus="")
 
         try:
@@ -186,63 +259,29 @@ class JanelaPrincipal:
             show_frame()
 
         except:
-            self.texto_mensagem = tk.Message(self.frame_principal)
-            self.texto_mensagem.place(relx=0.309, rely=0.285, relheight=0.292, relwidth=0.341)
-            self.texto_mensagem.configure(background="#d9d9d9")
-            self.texto_mensagem.configure(font=self.font9)
-            self.texto_mensagem.configure(foreground="#000000")
-            self.texto_mensagem.configure(highlightbackground="#d9d9d9")
-            self.texto_mensagem.configure(highlightcolor="black")
-            self.texto_mensagem.configure(text="Ocorreu um erro! Sua webcam não está disponível.")
-            self.texto_mensagem.configure(width=210)
+            self.tela_de_aviso(frame_principal, "Ocorreu um erro! Sua webcam não está disponível.")
 
     def botao_tirar_foto_clicado(self, frame_principal, botao_tirar_foto):
         self.continuar_mostrando_webcam = False
-        #img_name = "opencv_frame.png"
-        #cv2.imwrite(img_name, self.frame)
 
         self.botao_confirmar_registro = ttk.Button(self.frame_principal, text = "Confirmar registro", command = lambda: self.botao_confirmar_registro_clicado(self.frame_principal))
-        self.botao_confirmar_registro.place(relx=0.400, rely=0.870, height=35, width=116)
+        self.botao_confirmar_registro.place(relx=0.400, rely=0.892, height=35, width=116)
 
         self.botao_tirar_foto.destroy()
 
     def botao_confirmar_registro_clicado(self, frame_principal):
-        if self.validar_dados(self.texto_nome.get(), self.texto_email.get(), self.texto_cpf.get()):
+        # Tem que salvar em variável, porque toda vez que tu da 'get()' ele limpa o local de origem.
+        nome, email, cpf = self.texto_nome.get(), self.texto_email.get(), self.texto_cpf.get()
+
+        if self.validar_dados(nome, email, cpf):
             for child in frame_principal.winfo_children():
                 child.destroy()
 
-            salvar_database()
+            self.salvar_database(nome, email, cpf)
 
-            self.texto_mensagem = tk.Message(self.frame_principal)
-            self.texto_mensagem.place(relx=0.309, rely=0.285, relheight=0.292, relwidth=0.341)
-            self.texto_mensagem.configure(background="#d9d9d9")
-            self.texto_mensagem.configure(font=self.font9)
-            self.texto_mensagem.configure(foreground="#000000")
-            self.texto_mensagem.configure(highlightbackground="#d9d9d9")
-            self.texto_mensagem.configure(highlightcolor="black")
-            self.texto_mensagem.configure(text="Você foi registrado com sucesso.")
-            self.texto_mensagem.configure(width=210)
+            self.tela_de_aviso(frame_principal, "Você foi registrado com sucesso!")
         else:
-            self.frame_temp = ttk.Frame(self.frame_principal)
-            self.frame_temp.place(relx=0.014, rely=0.017, relheight=0.96, relwidth=0.97)
-            self.frame_temp.configure(relief='groove')
-            self.frame_temp.configure(borderwidth="2")
-            self.frame_temp.configure(relief="groove")
-
-            self.texto_mensagem = tk.Message(self.frame_temp)
-            self.texto_mensagem.place(relx = 0.309, rely = 0.285, relheight = 0.292, relwidth = 0.341)
-            self.texto_mensagem.configure(background = "#d9d9d9")
-            self.texto_mensagem.configure(font = self.font9)
-            self.texto_mensagem.configure(foreground = "#000000")
-            self.texto_mensagem.configure(highlightbackground = "#d9d9d9")
-            self.texto_mensagem.configure(highlightcolor = "black")
-            self.texto_mensagem.configure(text = "Você inseriu dados incorretos.")
-            self.texto_mensagem.configure(anchor='center')
-            self.texto_mensagem.configure(justify='center')
-            self.texto_mensagem.configure(width = 210)
-
-            self.botao_ok = ttk.Button(self.frame_temp, text = "Ok", command = self.frame_temp.destroy)
-            self.botao_ok.place(relx=0.400, rely=0.870, height=35, width=116)
+            self.tela_de_aviso(frame_principal, "Você inseriu dados incorretos.")
 
     # Vai retornar 'True' se todos os três forem válidos; 'False' caso algum deles não bata.
     def validar_dados(self, nome, email, cpf):
@@ -325,16 +364,30 @@ class JanelaPrincipal:
         print("cpf true")
         return True
 
-    def salvar_database(self):
-        conn = None
-        try:
-            conn = sqlite3.connect("../db")
-            print(sqlite3.version)
-        except Error as e:
-            print(e)
-        finally:
-            if conn:
-                conn.close()
+    def salvar_database(self, nome, email, cpf):
+        def adapt_array(arr):
+            out = io.BytesIO()
+            np.save(out, arr)
+            out.seek(0)
+            return sqlite3.Binary(out.read())
+
+        def convert_array(text):
+            out = io.BytesIO(text)
+            out.seek(0)
+            return np.load(out)
+
+        conn = self.__conecxao_db()
+
+        sqlite3.register_adapter(np.ndarray, adapt_array)
+        sqlite3.register_converter("array", convert_array)
+
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS registros (nome text, email text, cpf int, arr array)")
+        cur.execute("INSERT INTO registros (nome, email, cpf, arr) VALUES (?, ?, ?, ?)", (nome, email, cpf, self.frame, ))
+        conn.commit()
+        conn.close()
+
+        cv2.destroyAllWindows()
 
     def botao_sair_clicado(self):
         sys.exit(0)
